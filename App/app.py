6a764fcf193c6bfb8a3d1e28421a3065eb698c1a
Mark <mark.mark.galashev@gmail.com>
2026-03-05 01:59:22 +0300

    update dynamic level tab

--- App/app.py
+++ App/app.py
@@ -1,5 +1,8 @@
 import streamlit as st
 
+# настройка страницы
+st.set_page_config(layout="wide")
+
 # Заголовок приложения
 st.title("Well Engineering Calculator")
 
@@ -13,6 +16,46 @@ tab1, tab2, tab3 = st.tabs([
 # содержимое первой вкладки
 with tab1:
     st.header("Dynamic Level Calculation")
+    # ---- Ввод параметров ----
+
+    H = st.number_input("Well depth H (m)", value=2500.0)
+
+    P_ann = st.number_input(
+        "Annulus pressure P_ann (MPa)", value=3.0
+    )
+
+    P_g = st.number_input(
+        "Gas pressure P_g (MPa)", value=0.2
+    )
+
+    rho = st.number_input(
+        "Fluid density (kg/m³)", value=850.0
+    )
+
+    # ---- Константа ----
+
+    g = 9.81
+
+    # ---- Расчёт ----
+
+    if st.button("Calculate dynamic level"):
+
+        # перевод MPa → Pa
+        P_ann_pa = P_ann * 1_000_000
+        P_g_pa = P_g * 1_000_000
+
+        # высота жидкостного столба
+        h = (P_ann_pa - P_g_pa) / (rho * g)
+
+        # динамический уровень
+        H_dyn = H - h
+
+        st.subheader("Results")
+
+        st.write(f"Fluid column height: {h:.2f} m")
+
+        st.write(f"Dynamic level: {H_dyn:.2f} m")
+
 
 # содержимое второй вкладки
 with tab2:
@@ -22,42 +65,3 @@ with tab2:
 with tab3:
     st.header("Productivity Index")
 
-# ---- Ввод параметров ----
-
-H = st.number_input("Well depth H (m)", value=2500.0)
-
-P_ann = st.number_input(
-    "Annulus pressure P_ann (MPa)", value=3.0
-)
-
-P_g = st.number_input(
-    "Gas pressure P_g (MPa)", value=0.2
-)
-
-rho = st.number_input(
-    "Fluid density (kg/m³)", value=850.0
-)
-
-# ---- Константа ----
-
-g = 9.81
-
-# ---- Расчёт ----
-
-if st.button("Calculate dynamic level"):
-
-    # перевод MPa → Pa
-    P_ann_pa = P_ann * 1_000_000
-    P_g_pa = P_g * 1_000_000
-
-    # высота жидкостного столба
-    h = (P_ann_pa - P_g_pa) / (rho * g)
-
-    # динамический уровень
-    H_dyn = H - h
-
-    st.subheader("Results")
-
-    st.write(f"Fluid column height: {h:.2f} m")
-
-    st.write(f"Dynamic level: {H_dyn:.2f} m")
